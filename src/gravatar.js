50bc5369b44a9a9c6cfd89290af7c85641e020d3
insideplant <kami.10pearl@gmail.com>
2021-11-14 01:09:16 +0900

    create TextField component

--- src/gravatar.js
+++ src/gravatar.js
@@ -1,9 +1,9 @@
 import crypto from 'crypto'
 
 export const gravatarPath = (string) => {
-  
+
   const lowerCaseString = string.trim().toLowerCase();
   const md5 = crypto.createHash('md5')
-  md5.update(lowerCaseString, 'binary').digest('hex')
+  const digest = md5.update(lowerCaseString, 'binary').digest('hex')
   return `https://www.gravatar.com/avatar/${digest}/?d=retro`;
 };
