30198517c515fac99bb68206cfaddf916a435f01
insideplant <kami.10pearl@gmail.com>
2021-11-14 18:47:28 +0900

    create app

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import {TextField} from '@material-ui/core'
 
+import { pushMessage } from "../firebase";
+
 const MessageField = ({ name, setText, text}) => {
   const [isComposed, setIsComposed] = useState(false);
   console.log({ text });
@@ -14,7 +16,7 @@ const MessageField = ({ name, setText, text}) => {
         const text = e.target.value;
         if(text === "") return;
         if( e.key === 'Enter') {
-          console.log('push message to firebase');
+          pushMessage({ name: 'zhong_zhi', text });
           setText("");
           e.preventDefault();
         }
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase';
+
+// import firebase from "firebase/app";
+// import "firebase/app";
+// import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBg_FUhLQ_oqTLtnjuLbAdBcSjKrhYsTP8",
+  authDomain: "idobatakaigi2-a9e03.firebaseapp.com",
+  databaseURL: "https://idobatakaigi2-a9e03-default-rtdb.firebaseio.com",
+  projectId: "idobatakaigi2-a9e03",
+  storageBucket: "idobatakaigi2-a9e03.appspot.com",
+  messagingSenderId: "287083912843",
+  appId: "1:287083912843:web:5961ec7f4bc134f0b37e8e"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const messageRef = database.ref('messages');
+
+export const pushMessage = ({ name, text }) => {
+  messageRef.push({name, text});
+};
